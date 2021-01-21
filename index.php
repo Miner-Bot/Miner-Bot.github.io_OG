b5c01483e54a19373a885cdec247b49d739b99ee
IcicleSavage <mjseybold22@gmail.com>
2021-01-21 16:35:58 -0700

    E

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require 'PHPMailerAutoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+$mail = new PHPMailer;
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "minersfeedback@gmail.com";
+$mail->Password   = "your-gmail-password";
+$mail->IsHTML(true);
+$mail->addAddress('mjseybold22@gmail.com', 'My Friend');
+$mail->setFrom('minersfeedback@gmail.com', 'MinerBot');
+$mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
+$mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$mail->Body = 'Hi! This is my first e-mail sent through PHPMailer.';
+$mail->MsgHTML($content); 
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Message has been sent.';
+}
