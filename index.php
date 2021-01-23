708a129c4220a680d3aea29418362aba2b706d64
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-01-22 19:14:11 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,9 @@
-<?php
-require 'PHPMailerAutoload.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer-master/src/Exception.php';
-require 'PHPMailer-master/src/PHPMailer.php';
-require 'PHPMailer-master/src/SMTP.php';
-$mail = new PHPMailer;
-$mail->IsSMTP();
-$mail->Mailer = "smtp";
-$mail->SMTPDebug  = 1;  
-$mail->SMTPAuth   = TRUE;
-$mail->SMTPSecure = "tls";
-$mail->Port       = 587;
-$mail->Host       = "smtp.gmail.com";
-$mail->Username   = "minersfeedback@gmail.com";
-$mail->Password   = "*****";
-$mail->IsHTML(true);
-$mail->addAddress($_POST['email'], 'My Friend');
-$mail->setFrom('minersfeedback@gmail.com', 'MinerBot');
-$mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
-$mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");
-$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
-$mail->Body = 'Hi! This is my first e-mail sent through PHPMailer.';
-$mail->MsgHTML($content); 
-if(!$mail->send()) {
-  echo 'Message was not sent.';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
-}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>How to put PHP in HTML - Simple Example</title>
+</head>
+<body>
+<h1><?php echo "This message is from server side." ?></h1>
+</body>
+</html>
